51b94d999dac16d9379e5fcaa423e61e8a9b6b65
fred-secludit <fred@secludit.com>
2011-08-16 13:37:02 +0000

    Add own method for generating a random 'alnum' character

--- lib/audit/lib/util/random_string.rb
+++ lib/audit/lib/util/random_string.rb
@@ -2,21 +2,31 @@
 # and open the template in the editor.
 
 # Ruby 1.8 does not contains sample() function
+# NB:
+#   - ruby 1.8.6 has no equivalent method
+#   - ruby 1.8.7 has choice() method
+#   - ruby 1.9 has sample() method
 class Array
-  def sample()
-    return self.choice() 
+  def sec_sample()
+    if self.respond_to?(:sample, true)
+      return self.sample()
+    elsif self.respond_to?(:choice, true)
+      return self.choice() 
+    else
+      return (('A' .. 'Z').to_a + ('a' .. 'z').to_a + ('0' .. '9').to_a)[Kernel.srand.modulo(62)]
+    end
   end
 end
 
 class RandomString
   def initialize
-    
   end
+
   def self.generate(length = 20, alphabet = ('A' .. 'Z').to_a + ('a' .. 'z').to_a + ('0' .. '9').to_a)
-    return (0 .. length).map { alphabet.sample }.join
+    return (0 .. length).map { alphabet.sec_sample }.join
   end
 
   def self.generate_name(length = 20)
-    return (('A' .. 'Z').to_a + ('a' .. 'z').to_a).sample + generate(length - 1)
+    return (('A' .. 'Z').to_a + ('a' .. 'z').to_a).sec_sample + generate(length - 1)
   end
 end
